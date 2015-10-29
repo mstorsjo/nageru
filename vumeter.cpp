ecc764902248055bf3a36ff9b541014779c1b907
Steinar H. Gunderson <sgunderson@bigfoot.com>
2015-10-29 21:26:38 +0100

    Various VU meter layout tweaks.

--- vumeter.cpp
+++ vumeter.cpp
@@ -8,8 +8,8 @@ namespace {
 
 int lufs_to_pos(float level_lu, int height)
 {
-	const float min_level = 18.0f;    // y=0 is top of screen, so “min” is the loudest level.
-	const float max_level = -36.0f;
+	const float min_level = 9.0f;    // y=0 is top of screen, so “min” is the loudest level.
+	const float max_level = -18.0f;
 	int y = lrintf(height * (level_lu - min_level) / (max_level - min_level));
 	y = std::max(y, 0);
 	y = std::min(y, height - 1);
@@ -32,9 +32,18 @@ void VUMeter::paintEvent(QPaintEvent *event)
 
 	painter.fillRect(0, 0, width(), height(), Qt::black);
 
-	int min_y = lufs_to_pos(1.0f, height());
-	int max_y = lufs_to_pos(-1.0f, height());
-	painter.fillRect(0, min_y, width(), max_y - min_y, Qt::green);
+	// Draw some reference bars.
+	for (int level = -18; level < 9; ++level) {
+		int min_y = lufs_to_pos(level, height()) - 1;
+		int max_y = lufs_to_pos(level + 1.0f, height()) + 1;
+
+		// Recommended range is 0 LU +/- 1 LU.
+		if (level == -1 || level == 0) {
+			painter.fillRect(1, min_y, width() - 2, max_y - min_y, Qt::green);
+		} else {
+			painter.fillRect(1, min_y, width() - 2, max_y - min_y, Qt::darkGray);
+		}
+	}
 
 	float level_lufs;
 	{
@@ -44,6 +53,5 @@ void VUMeter::paintEvent(QPaintEvent *event)
 
 	float level_lu = level_lufs + 23.0f;
 	int y = lufs_to_pos(level_lu, height());
-	painter.setPen(Qt::white);
-	painter.drawLine(0, y, width(), y);
+	painter.fillRect(0, y, width(), 2, Qt::white);
 }
