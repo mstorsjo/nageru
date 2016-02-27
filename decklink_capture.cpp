1a77b757e536a12d47b0414fefc7295b2d31079f
Steinar H. Gunderson <sgunderson@bigfoot.com>
2016-02-27 16:56:07 +0100

    Add AVX2 and SSE2 fastpaths to the DeckLink PCI driver.

--- decklink_capture.cpp
+++ decklink_capture.cpp
@@ -6,6 +6,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <cstddef>
+#ifdef __SSE2__
+#include <immintrin.h>
+#endif
 
 #include <DeckLinkAPI.h>
 #include <DeckLinkAPIConfiguration.h>
@@ -21,7 +24,6 @@ using namespace std::placeholders;
 namespace {
 
 // TODO: Support stride.
-// TODO: Support AVX2 (adapt from bmusb).
 void memcpy_interleaved(uint8_t *dest1, uint8_t *dest2, const uint8_t *src, size_t n)
 {
 	assert(n % 2 == 0);
@@ -33,6 +35,101 @@ void memcpy_interleaved(uint8_t *dest1, uint8_t *dest2, const uint8_t *src, size
 		*dptr2++ = *src++;
 	}
 }
+
+#ifdef __SSE2__
+
+// Returns the number of bytes consumed.
+size_t memcpy_interleaved_fastpath(uint8_t *dest1, uint8_t *dest2, const uint8_t *src, size_t n)
+{
+	const uint8_t *limit = src + n;
+	size_t consumed = 0;
+
+	// Align end to 32 bytes.
+	limit = (const uint8_t *)(intptr_t(limit) & ~31);
+
+	if (src >= limit) {
+		return 0;
+	}
+
+	// Process [0,31] bytes, such that start gets aligned to 32 bytes.
+	const uint8_t *aligned_src = (const uint8_t *)(intptr_t(src + 31) & ~31);
+	if (aligned_src != src) {
+		size_t n2 = aligned_src - src;
+		memcpy_interleaved(dest1, dest2, src, n2);
+		dest1 += n2 / 2;
+		dest2 += n2 / 2;
+		if (n2 % 1) {
+			swap(dest1, dest2);
+		}
+		src = aligned_src;
+		consumed += n2;
+	}
+
+	// Make the length a multiple of 64.
+	if (((limit - src) % 64) != 0) {
+		limit -= 32;
+	}
+	assert(((limit - src) % 64) == 0);
+
+#if __AVX2__
+	const __restrict __m256i *in = (const __m256i *)src;
+	__restrict __m256i *out1 = (__m256i *)dest1;
+	__restrict __m256i *out2 = (__m256i *)dest2;
+
+	__m256i shuffle_cw = _mm256_set_epi8(
+		15, 13, 11, 9, 7, 5, 3, 1, 14, 12, 10, 8, 6, 4, 2, 0,
+		15, 13, 11, 9, 7, 5, 3, 1, 14, 12, 10, 8, 6, 4, 2, 0);
+	while (in < (const __m256i *)limit) {
+		// Note: For brevity, comments show lanes as if they were 2x64-bit (they're actually 2x128).
+		__m256i data1 = _mm256_stream_load_si256(in);         // AaBbCcDd EeFfGgHh
+		__m256i data2 = _mm256_stream_load_si256(in + 1);     // IiJjKkLl MmNnOoPp
+
+		data1 = _mm256_shuffle_epi8(data1, shuffle_cw);       // ABCDabcd EFGHefgh
+		data2 = _mm256_shuffle_epi8(data2, shuffle_cw);       // IJKLijkl MNOPmnop
+	
+		data1 = _mm256_permute4x64_epi64(data1, 0b11011000);  // ABCDEFGH abcdefgh
+		data2 = _mm256_permute4x64_epi64(data2, 0b11011000);  // IJKLMNOP ijklmnop
+
+		__m256i lo = _mm256_permute2x128_si256(data1, data2, 0b00100000);
+		__m256i hi = _mm256_permute2x128_si256(data1, data2, 0b00110001);
+
+		_mm256_storeu_si256(out1, lo);
+		_mm256_storeu_si256(out2, hi);
+
+		in += 2;
+		++out1;
+		++out2;
+		consumed += 64;
+	}
+#else
+	const __restrict __m128i *in = (const __m128i *)src;
+	__restrict __m128i *out1 = (__m128i *)dest1;
+	__restrict __m128i *out2 = (__m128i *)dest2;
+
+	__m128i mask_lower_byte = _mm_set1_epi16(0x00ff);
+	while (in < (const __m128i *)limit) {
+		__m128i data1 = _mm_load_si128(in);
+		__m128i data2 = _mm_load_si128(in + 1);
+		__m128i data1_lo = _mm_and_si128(data1, mask_lower_byte);
+		__m128i data2_lo = _mm_and_si128(data2, mask_lower_byte);
+		__m128i data1_hi = _mm_srli_epi16(data1, 8);
+		__m128i data2_hi = _mm_srli_epi16(data2, 8);
+		__m128i lo = _mm_packus_epi16(data1_lo, data2_lo);
+		_mm_storeu_si128(out1, lo);
+		__m128i hi = _mm_packus_epi16(data1_hi, data2_hi);
+		_mm_storeu_si128(out2, hi);
+
+		in += 2;
+		++out1;
+		++out2;
+		consumed += 32;
+	}
+#endif
+
+	return consumed;
+}
+
+#endif  // __SSE2__
 
 }  // namespace
 
@@ -167,9 +264,25 @@ HRESULT STDMETHODCALLTYPE DeckLinkCapture::VideoInputFrameArrived(
 			const uint8_t *frame_bytes;
 			video_frame->GetBytes((void **)&frame_bytes);
 
-			memcpy_interleaved(current_video_frame.data, current_video_frame.data2,
-				frame_bytes, width * height * 2);
-			current_video_frame.len += width * height * 2;
+			uint8_t *data = current_video_frame.data;
+			uint8_t *data2 = current_video_frame.data2;
+			size_t num_bytes = width * height * 2;
+#ifdef __SSE2__
+			size_t consumed = memcpy_interleaved_fastpath(data, data2, frame_bytes, num_bytes);
+			frame_bytes += consumed;
+			data += consumed / 2;
+			data2 += consumed / 2;
+			if (num_bytes % 2) {
+				swap(data, data2);
+			}
+			current_video_frame.len += consumed;
+			num_bytes -= consumed;
+#endif
+
+			if (num_bytes > 0) {
+				memcpy_interleaved(data, data2, frame_bytes, num_bytes);
+			}
+			current_video_frame.len += num_bytes;
 
 			video_format.width = width;
 			video_format.height = height;
