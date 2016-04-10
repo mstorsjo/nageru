b6ef73048955b70c9c3653525fd840c6cc1187ef
Steinar H. Gunderson <sgunderson@bigfoot.com>
2016-04-10 15:21:15 +0200

    Add usage information on invalid option or --help.

--- flags.cpp
+++ flags.cpp
@@ -6,9 +6,23 @@
 
 Flags global_flags;
 
+void usage()
+{
+	fprintf(stderr, "Usage: nageru [OPTION]...\n");
+	fprintf(stderr, "\n");
+	fprintf(stderr, "  -h, --help                      print usage information\n");
+	fprintf(stderr, "  -c, --num-cards                 set number of input cards (default 2)\n");
+	fprintf(stderr, "  -t, --theme=FILE                choose theme (default theme.lua)\n");
+	fprintf(stderr, "  -v, --va-display=SPEC           VA-API device for H.264 encoding\n");
+	fprintf(stderr, "                                    ($DISPLAY spec or /dev/dri/render* path)\n");
+	fprintf(stderr, "      --http-uncompressed-video   send uncompressed NV12 video to HTTP clients\n");
+	fprintf(stderr, "      --flat-audio                start with most audio processing turned off\n");
+}
+
 void parse_flags(int argc, char * const argv[])
 {
 	static const option long_options[] = {
+		{ "help", no_argument, 0, 'h' },
 		{ "num-cards", required_argument, 0, 'c' },
 		{ "theme", required_argument, 0, 't' },
 		{ "va-display", required_argument, 0, 1000 },
@@ -39,8 +53,13 @@ void parse_flags(int argc, char * const argv[])
 		case 1002:
 			global_flags.flat_audio = true;
 			break;
+		case 'h':
+			usage();
+			exit(0);
 		default:
 			fprintf(stderr, "Unknown option '%s'\n", argv[option_index]);
+			fprintf(stderr, "\n");
+			usage();
 			exit(1);
 		}
 	}
